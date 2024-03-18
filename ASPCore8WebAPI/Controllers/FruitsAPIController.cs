adf0e9c39b7549a82bd887ebbc930e2ebd3a0c53
waqar <wakku9780@gmail.com>
2024-03-19 00:48:43 +0530

    Add project files.

---
+++ ASPCore8WebAPI/Controllers/FruitsAPIController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCore8WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsAPIController : ControllerBase
+    {
+        public List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Banana",
+            "Mango",
+            "Cherry",
+            "Grapes"
+
+        };
+        [HttpGet]
+        public List<string> GetFruits()// yah action method hai yah action mehthod tab execute hoga, jab iske pass get request generate hogi 
+        {
+            return fruits;
+        }
+
+        [HttpGet("{id}")]
+        public string GetFruitsByIndex(int id)// yah action method hai yah action mehthod tab execute hoga, jab iske pass get request generate hogi 
+        {
+            return fruits.ElementAt(id);
+        }
+    }
+}
